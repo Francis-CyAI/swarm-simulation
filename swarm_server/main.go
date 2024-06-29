b5232c97fceb14a10a44e62cc3877f94b184cea2
Francis Kalunga <kalungafrancis28@gmail.com>
2024-06-30 00:39:24 +0200

    Now able to update point location. Need to fix infinite loop problem

--- swarm_server/main.go
+++ swarm_server/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"swarm_server/space"
-	// "swarm_server/node"
+	"swarm_server/node"
 	"fmt"
 	// "os"
 )
@@ -11,6 +11,25 @@ func main() {
 	// fmt.Println("\nWelcome to the CLI version of Swarm Simulation")
 	var sp space.Plane
 	sp_o := sp.Origin // (0, 0, 0) by default
+	sp.End.X, sp.End.Y, sp.End.Z = 1, 1, 100
 	
-	fmt.Print(sp_o.X)
+	var n node.Node
+
+	// fmt.Printf("Node in main, before: %d, %d, %d\n", n.P.X, n.P.Y, n.P.Z)
+
+	n.P = sp_o
+
+	// fmt.Printf("Node in main, after: %d, %d, %d\n", n.P.X, n.P.Y, n.P.Z)
+
+	occupiedSpace := []space.Point{sp_o}
+	var done bool
+
+	for {
+		fmt.Printf("Current point is (%d, %d, %d)\n", n.P.X, n.P.Y, n.P.Z)
+
+		if occupiedSpace, done = n.MoveTo(occupiedSpace, sp.End); done {
+			fmt.Printf("Node in main, now at: %d, %d, %d\n", n.P.X, n.P.Y, n.P.Z)
+			return
+		}
+	}
 }
--- swarm_server/node/node.go
+++ swarm_server/node/node.go
@@ -1,36 +1,71 @@
 package node
 
 import (
+	"fmt" // TODO: remove
+	"slices"
 	"swarm_server/space"
 	"sync"
-	"slices"
 )
 
 type Node struct {
-	R float32 /// Radius
+	R float32     /// Radius
 	P space.Point /// Position of center
 }
 
-func (node *Node) MoveTo(space []space.Point) []space.Point {
-	/**
-		Node must first check if point exists in
-		the occupied points Slice. If it does, it
-		chooses a different point.
-	*/
-	var mu sync.Mutex
-	
-	mu.Lock()	
-	node.P = NextAvailableSpacePoint(space, node.P)
-	mu.Unlock()
-	
-	// remove previous point
-	pIndex := slices.Index(space, node.P)
-	space = slices.Delete(space, pIndex, pIndex + 1)
+func (node Node) IndexIn(space []space.Point) (bool, int) {
+	there := false
+	i := -1
 
-	return space
+	for j := 0; j < len(space); j++ {
+		/*
+		fmt.Printf("space point = ")
+		fmt.Printf(space[j])
+		fmt.Printf("\nnode point = ")
+		fmt.Printf(node.P)
+		*/
+		fmt.Printf("space[j] (space[%d]): X = %d, Y = %d, Z = %d\n", j, space[j].X, space[j].Y, space[j].Z)
+		fmt.Printf("node: X = %d, Y = %d, Z = %d\n \n", node.P.X, node.P.Y, node.P.Z)
+
+		if currSPoint := space[j]; node.P.X == currSPoint.X && node.P.Y == currSPoint.Y && node.P.Z == currSPoint.Z {
+			there = true
+			i = j
+		}
+	}
+
+	return there, i
 }
 
-func NextAvailableSpacePoint(space []space.Point, p space.Point) space.Point {
+func (node Node) MoveTo(space []space.Point, limit space.Point) ([]space.Point, bool) { // TODO: fix logic
+	/**
+	Node must first check if point exists in
+	the occupied points Slice. If it does, it
+	chooses a different point.
+	*/
+
+	var mu sync.Mutex
+	var done bool
+	mu.Lock()
+	node.P, done = NextAvailableSpacePoint(space, node.P, limit)
+	mu.Unlock()
+
+	// remove previous point
+	there, pIndex := node.IndexIn(space)
+	if there {
+		space = slices.Delete(space, pIndex, pIndex+1)
+	}
+	// fmt.Printf("pIndex = %d, pIndex + 1 = %d", pIndex, pIndex+1)
+	// fmt.Printf("\tspace length = %d\n", len(space))
+
+	return space, done
+}
+
+func NextAvailableSpacePoint(space []space.Point, p space.Point, limit space.Point) (space.Point, bool) {
+	done := true
+
+	if p.X >= limit.X && p.Y >= limit.Y && p.Z >= limit.Z {
+		return p, done
+	} // limit reached
+
 	if p.X <= p.Y || p.X <= p.Z {
 		p.X += 1
 	} else if p.Y <= p.X || p.Y <= p.Z {
@@ -41,8 +76,8 @@ func NextAvailableSpacePoint(space []space.Point, p space.Point) space.Point {
 
 	if pointNotFree := slices.Contains(space, p); pointNotFree {
 		// Point is not free
-		NextAvailableSpacePoint(space, p)
+		// NextAvailableSpacePoint(space, p, limit)
 	}
 
-	return p
+	return p, !done
 }
