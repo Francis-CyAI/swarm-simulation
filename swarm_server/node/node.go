6a212e5846e3f13c38043f2c7cce19ae97a9b6cf
Francis Kalunga <kalungafrancis28@gmail.com>
2024-06-29 22:58:29 +0200

    Done with node navigation logic

--- swarm_server/node/node.go
+++ swarm_server/node/node.go
@@ -11,7 +11,7 @@ type Node struct {
 	P space.Point /// Position of center
 }
 
-func (node *Node) MoveTo(space []space.Point, p space.Point){
+func (node *Node) MoveTo(space []space.Point) []space.Point {
 	/**
 		Node must first check if point exists in
 		the occupied points Slice. If it does, it
@@ -20,9 +20,29 @@ func (node *Node) MoveTo(space []space.Point, p space.Point){
 	var mu sync.Mutex
 	
 	mu.Lock()	
-	if pointNotFree := slices.Contains(space, p); !pointNotFree {
-		node.P = p
-	}
-	defer mu.Unlock()
+	node.P = NextAvailableSpacePoint(space, node.P)
+	mu.Unlock()
+	
+	// remove previous point
+	pIndex := slices.Index(space, node.P)
+	space = slices.Delete(space, pIndex, pIndex + 1)
 
+	return space
+}
+
+func NextAvailableSpacePoint(space []space.Point, p space.Point) space.Point {
+	if p.X <= p.Y || p.X <= p.Z {
+		p.X += 1
+	} else if p.Y <= p.X || p.Y <= p.Z {
+		p.Y += 1
+	} else {
+		p.Z += 1
+	}
+
+	if pointNotFree := slices.Contains(space, p); pointNotFree {
+		// Point is not free
+		NextAvailableSpacePoint(space, p)
+	}
+
+	return p
 }
