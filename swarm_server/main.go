8e7a77a8d123a0a3e255e8f04836a236a528a56a
Francis Kalunga <kalungafrancis28@gmail.com>
2024-07-03 19:30:33 +0200

    Core logic working perfectly. Now need to instatiate multiple nodes in parallel

--- swarm_server/main.go
+++ swarm_server/main.go
@@ -8,26 +8,38 @@ import (
 )
 
 func main() {
-	// fmt.Println("\nWelcome to the CLI version of Swarm Simulation")
-	var sp space.Plane
-	sp_o := sp.Origin // (0, 0, 0) by default
-	sp.End.X, sp.End.Y, sp.End.Z = 1, 1, 100
+	/*
+		// fmt.Println("\nWelcome to the CLI version of Swarm Simulation")
+		var sp space.Plane
+		sp_o := sp.Origin // (0, 0, 0) by default
+		sp.End.X, sp.End.Y, sp.End.Z = 1, 1, 100
 
+		var n node.Node
+
+		// fmt.Printf("Node in main, before: %d, %d, %d\n", n.P.X, n.P.Y, n.P.Z)
+
+		n.P = sp_o
+
+		// fmt.Printf("Node in main, after: %d, %d, %d\n", n.P.X, n.P.Y, n.P.Z)
+
+		occupiedSpace := []space.Point{sp_o}
+		var done bool
+
+		fmt.Printf("Current point is (%d, %d, %d)\n", n.P.X, n.P.Y, n.P.Z)
+
+		if occupiedSpace, done = n.MoveTo(occupiedSpace, sp.End); done {
+			fmt.Printf("Node in main, now at: %d, %d, %d\n", n.P.X, n.P.Y, n.P.Z)
+			return
+		}
+	*/
+
+	tester()
+}
+
+func tester() {
 	var n node.Node
+	limit := space.Point{X: 1, Y: 1, Z: 20}
+	n.MoveTo([]space.Point{}, limit)
+	fmt.Printf("n initial values; X: %d, Y: %d, Z: %d", n.P.X, n.P.Y, n.P.Z)
 
-	// fmt.Printf("Node in main, before: %d, %d, %d\n", n.P.X, n.P.Y, n.P.Z)
-
-	n.P = sp_o
-
-	// fmt.Printf("Node in main, after: %d, %d, %d\n", n.P.X, n.P.Y, n.P.Z)
-
-	occupiedSpace := []space.Point{sp_o}
-	var done bool
-
-	fmt.Printf("Current point is (%d, %d, %d)\n", n.P.X, n.P.Y, n.P.Z)
-
-	if occupiedSpace, done = n.MoveTo(occupiedSpace, sp.End); done {
-		fmt.Printf("Node in main, now at: %d, %d, %d\n", n.P.X, n.P.Y, n.P.Z)
-		return
-	}
 }
--- swarm_server/node/node.go
+++ swarm_server/node/node.go
@@ -1,7 +1,7 @@
 package node
 
 import (
-	// TODO: remove
+	"fmt" // TODO: remove
 	"slices"
 	"swarm_server/space"
 	"sync"
@@ -12,7 +12,7 @@ type Node struct {
 	P space.Point /// Position of center
 }
 
-func (node Node) IndexIn(space []space.Point) (bool, int) {
+func (node Node) IndexIn(space []space.Point) (bool, int) { // Works fine
 	there := false
 	i := -1
 	for j := 0; j < len(space); j++ {
@@ -25,7 +25,6 @@ func (node Node) IndexIn(space []space.Point) (bool, int) {
 	return there, i
 }
 
-/*
 func indexIn(space []space.Point, p space.Point) (bool, int) {
 	there := false
 	i := -1
@@ -38,13 +37,14 @@ func indexIn(space []space.Point, p space.Point) (bool, int) {
 
 	return there, i
 }
-*/
 
 func nextPoint(currPoint space.Point, limit space.Point) (space.Point, bool) {
+	//  Okay: Increments fine, and gives the right done status.
+
 	// return next point, and done status
 
 	var done bool
-	
+
 	if limit.X >= limit.Y && limit.X >= limit.Z {
 		// X is the main dimension
 		currPoint.X++
@@ -59,65 +59,122 @@ func nextPoint(currPoint space.Point, limit space.Point) (space.Point, bool) {
 		done = currPoint.Z >= limit.Z
 	}
 
-
 	return currPoint, done
 }
 
-func (node Node) MoveTo(space []space.Point, limit space.Point) ([]space.Point, bool) { // TODO: fix logic
+func (node *Node) MoveTo(space []space.Point, limit space.Point) ([]space.Point, bool) { // TODO: fix logic
+	// Okay: working perfectly
+
 	/**
 	Node must first check if point exists in
 	the occupied points Slice. If it does, it
 	chooses a different point within the nearest space.
 	*/
 
-	// free previously occupied point
-	
-	if there, pIndex := node.IndexIn(space); there {
-		// fmt.Printf("Node is there in space at index %d, now removing it\n", pIndex)
-		space = slices.Delete(space, pIndex, pIndex+1)
-		// there, _ := node.IndexIn(space)
-		// fmt.Printf("There: %t\n", there)
-	}
-
 	var mu sync.Mutex
 
 	var done bool
-	mu.Lock()
-	node.P, done = nextPoint(node.P, limit)
-	mu.Unlock()
 
-	there, _ := node.IndexIn(space)
+	for {
 
-	if there /* collision has occured */ {
-		node.P = NextAvailableSpacePoint(space, node.P)
+		// free previously occupied point
+		if there, pIndex := node.IndexIn(space); there {
+			// fmt.Printf("Node is there in space at index %d, now removing it\n", pIndex)
+			space = slices.Delete(space, pIndex, pIndex+1)
+			// there, _ := node.IndexIn(space)
+			// fmt.Printf("There: %t\n", there)
+		}
+
+		mu.Lock()
+		node.P, done = nextPoint(node.P, limit)
+		mu.Unlock()
+		fmt.Printf("nextPoint:- X: %d, Y: %d, Z: %d; done: %t\n", node.P.X, node.P.Y, node.P.Z, done)
+
+		there, _ := node.IndexIn(space)
+
+		if there /* collision has occured */ {
+			notFound := true
+			for {
+				node.P, notFound = NextAvailableSpacePoint(space, node.P, limit)
+				if !notFound {
+					break
+				} // A free space point has been found
+				//TODO: if space vicinity exhausted, move to 'nextPoint'
+			}
+		}
+
+		// fmt.Printf("pIndex = %d, pIndex + 1 = %d", pIndex, pIndex+1)
+		// fmt.Printf("\tspace length = %d\n", len(space))
+		if done {
+			break
+		}
 	}
-
-	// fmt.Printf("pIndex = %d, pIndex + 1 = %d", pIndex, pIndex+1)
-	// fmt.Printf("\tspace length = %d\n", len(space))
-
 	return space, done
 }
 
-func NextAvailableSpacePoint(space []space.Point, p space.Point) space.Point {
+func NextAvailableSpacePoint(space []space.Point, p space.Point, limit space.Point) (space.Point, bool) {
 	/*
-		Finds the nearest point with the space of [0, 1] for any of the points as needed.
+		Finds the nearest point with the limit for any of the secondary axes
 	*/
+
+	notFound := true
 	availP := p
 
-	if availP.X < availP.Y || availP.X < availP.Z {
-		availP.X++
-	} else if availP.Y < availP.X || availP.Y < availP.Z {
-		availP.Y++
-	} else {
-		availP.Z++
+	if availP.X > availP.Y && availP.X > availP.Z {
+		// Exclude X as it is the main axis
+		if availP.Y < availP.Z && availP.Y < limit.Y {
+			availP.Y++
+			if occupied, _ := indexIn(space, availP); occupied {
+				NextAvailableSpacePoint(space, availP, limit)
+			}
+		} else if availP.Z < limit.Z {
+			availP.Z++
+			if occupied, _ := indexIn(space, availP); occupied {
+				NextAvailableSpacePoint(space, availP, limit)
+			}
+		} else {
+			// not available space point found
+			return p, notFound
+		}
+	} else if availP.Y > availP.X && availP.Y > availP.Z {
+		// Exclude Y as it is the main axis
+		if availP.X < availP.Z && availP.X < limit.X {
+			availP.X++
+			if occupied, _ := indexIn(space, availP); occupied {
+				NextAvailableSpacePoint(space, availP, limit)
+			}
+		} else if availP.Z < limit.Z {
+			availP.Z++
+			if occupied, _ := indexIn(space, availP); occupied {
+				NextAvailableSpacePoint(space, availP, limit)
+			}
+		} else {
+			return p, notFound
+		}
+	} else if availP.Z > availP.X && availP.Z > availP.Y {
+		// Exclude Z as it is the main axis
+		if availP.X < availP.Y && availP.X < limit.X {
+			availP.X++
+			if occupied, _ := indexIn(space, availP); occupied {
+				NextAvailableSpacePoint(space, availP, limit)
+			}
+		} else if availP.Y < limit.Y {
+			availP.Y++
+			if occupied, _ := indexIn(space, availP); occupied {
+				NextAvailableSpacePoint(space, availP, limit)
+			}
+		} else {
+			return p, notFound
+		}
 	}
 
-	
+	/*
+		for there := slices.Contains(space, availP) /* ?? May not work / ; there == true; {
+			fmt.Printf("availP X = %d, Y = %d, Z = %d; there = %t", availP.X, availP.Y, availP.Z, there)
+			// run to find next available point as long no free point i.e., !there, is found
+			NextAvailableSpacePoint(space, availP)
+		}
+	*/
 
-	for there := slices.Contains(space, availP) /* ?? May not work */ ; there == true ; {
-		// run to find next available point as long no free point i.e., !there, is found
-		NextAvailableSpacePoint(space, availP)
-	}
-
-	return availP // Not exactly useful, func just needs to return. Actual return statement at the top
+	return availP, !notFound // Found a free space point
 }
